5d6ddc33b72d90bcbbcad0a9a15dc7fb87a3ab71
Bhav Beri <bhavberi@gmail.com>
2024-03-10 20:44:05 +0530

    Setup logout callback

--- web/src/utils/auth.jsx
+++ web/src/utils/auth.jsx
@@ -10,12 +10,17 @@ export function login(pathname) {
   window.location.replace("https://clubs.iiit.ac.in/login");
 }
 
-export function logout(pathname) {
+export async function logout(pathname) {
   // save path to continue from
   setCookie("continue", pathname);
 
   // set flag to expire token the next time someone visits the site, because CAS doesn't follow ?service for some reason
-  setCookie("logout", true);
+  // setCookie("logout", true);
+
+  // console.log("Logging out...");
+
+  // fetch logout page to expire token
+  await fetch("https://clubs.iiit.ac.in/logoutCallback");
 
   // redirect to CAS logout
   window.location.replace("https://clubs.iiit.ac.in/logout");
