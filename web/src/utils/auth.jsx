e27e70debf556b78af9fe627a9829c73cc11916d
bhavberi <bhavberi@users.noreply.github.com>
2024-03-17 06:19:01 +0000

    Apply Prettier Formatting Fixes

--- web/src/utils/auth.jsx
+++ web/src/utils/auth.jsx
@@ -7,7 +7,9 @@ export function login(pathname) {
   setCookie("continue", pathname);
 
   // redirect to CAS login
-  window.location.replace("https://clubs.iiit.ac.in/login/https://clubs.iiit.ac.in");
+  window.location.replace(
+    "https://clubs.iiit.ac.in/login/https://clubs.iiit.ac.in",
+  );
 }
 
 export async function logout(pathname) {
