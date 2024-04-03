1101c98d38c5f6955ddfcb86b0692e7420d3f690
Bhav Beri <bhavberi@gmail.com>
2024-04-03 17:30:52 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-IP_ADDRESS=$(hostname -I | awk '{print $1}')
-
 # Write environment variables to .env file
 cat << EOF > ./web/.env
 GRAPHQL_ENDPOINT="https://clubs.iiit.ac.in/graphql"
@@ -10,10 +8,13 @@ NEXT_PUBLIC_STATIC_URL="https://clubs.iiit.ac.in/static"
 NEXT_PUBLIC_CLUB_ID="hacking.club"
 EOF
 
-FILE_PATH="web/next.config.js"
-
-# Update the IP address in the file
-sed -Ei "s|http://10\.[0-9]+\.[0-9]+\.[0-9]+|https://clubs.iiit.ac.in|g" $FILE_PATH
+# IP_ADDRESS=$(hostname -I | awk '{print $1}')
+# cat << EOF > ./web/.env
+# GRAPHQL_ENDPOINT="http://${IP_ADDRESS}/graphql"
+# NEXT_PUBLIC_FILESERVER_URL="http://${IP_ADDRESS}"
+# NEXT_PUBLIC_STATIC_URL="http://${IP_ADDRESS}/static"
+# NEXT_PUBLIC_CLUB_ID="osdg"
+# EOF
 
 # cd web
 # npm i
