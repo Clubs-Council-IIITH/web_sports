409d2d851dc530495b3f7df1119b627cba0cf867
Bhav Beri <bhavberi@gmail.com>
2024-05-25 13:31:06 +0530

    Updated Footer Text/Link

--- web/src/components/Footer.jsx
+++ web/src/components/Footer.jsx
@@ -154,11 +154,11 @@ export default function Footer({ club = {} }) {
               // },
             }}
           >
-            Developed & Maintained with ❤️ by SLC Tech Team (powered by{" "}
+            Developed & Maintained with ❤️ by{" "}
             <Typography
               variant="body2"
               component={Link}
-              href={"https://clubs.iiit.ac.in/"}
+              href={"https://clubs.iiit.ac.in/about/clubs-council/tech-members"}
               sx={{
                 fontWeight: 500,
                 textDecoration: "none",
@@ -168,9 +168,9 @@ export default function Footer({ club = {} }) {
                 },
               }}
             >
-              Clubs Council
-            </Typography>
-            )
+              SLC Tech Team
+            </Typography>{" "}
+            (powered by Clubs Council)
           </Typography>
         </Grid>
       </Grid>
